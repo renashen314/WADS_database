ba3e4d5cbae144e414769d56879a3633c03d5b50
Rena Shen <renashen@m-qjy491y6y9.local>
2023-06-15 16:21:17 -0400

    FIX: change 2 fields in webapp migration file, partials, controller, and re-migrated db

--- app/controllers/webapps_controller.rb
+++ app/controllers/webapps_controller.rb
@@ -65,6 +65,6 @@ class WebappsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def webapp_params
-      params.require(:webapp).permit(:project_name, :language, :framework, :description, :notes, :status, :authentication, :database, :server_os, :server_location, :documentation, :git_repo, :app_url, :risk_manage_consideration, :start_date, :end_date)
+      params.require(:webapp).permit(:project_name, :language, :framework, :description, :notes, :status, :authentication, :database, :server_os, :server_location, :documentation, :git_repo, :app_url, :risk_manage_consideration, :launch_date, :end_of_life_date)
     end
 end
--- app/views/webapps/_webapp.json.jbuilder
+++ app/views/webapps/_webapp.json.jbuilder
@@ -1,2 +1,2 @@
-json.extract! webapp, :id, :project_name, :language, :framework, :description, :notes, :status, :authentication, :database, :server_os, :server_location, :documentation, :git_repo, :app_url, :risk_manage_consideration, :start_date, :end_date, :created_at, :updated_at
+json.extract! webapp, :id, :project_name, :language, :framework, :description, :notes, :status, :authentication, :database, :server_os, :server_location, :documentation, :git_repo, :app_url, :risk_manage_consideration, :launch_date, :end_of_life_date, :created_at, :updated_at
 json.url webapp_url(webapp, format: :json)
