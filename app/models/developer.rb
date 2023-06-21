1397cc040a9b52ebf5415d7a1f921308c98cb8b5
Debbie Shih <dcjshih@umich.edu>
2023-06-21 14:05:49 -0400

    ADD: Developer and webapp tables are now visually connected. Able to navigate from webapp to developer

--- app/models/developer.rb
+++ app/models/developer.rb
@@ -1,4 +1,8 @@
 class Developer < ApplicationRecord
     has_many :dev_apps, dependent: :destroy
     has_many :webapps, through: :dev_apps
+
+    def full_name
+        "#{first_name} #{last_name}"
+    end
 end
