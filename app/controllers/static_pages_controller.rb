230704f9bd1be83b4e185e2efb03c217567fddc5
Debbie Shih <dcjshih@umich.edu>
2023-08-02 14:01:08 -0400

    Fixing validation

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,6 @@
 class StaticPagesController < ApplicationController
     skip_before_action :authenticate_user!, only: [:homepage, :about]
     def homepage
+
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
   rolify
-  has_one :developer, dependent: :destroy
+  has_one :developer, :dependent => :destroy
   after_create :create_dev
   validate :check_mcommunity_group
   # Include default devise modules. Others available are:
@@ -17,8 +17,7 @@ class User < ApplicationRecord
   private
   def check_mcommunity_group
     uniq = self.email.split("@").first
-    errors.add(:email, "must be in W&ADS") unless LdapLookup.is_member_of_group?(uniq, "LSA TS Web and Application Development Services
-      ")
+    errors.add(:email, "must be in W&ADS") unless LdapLookup.is_member_of_group?(uniq, "LSA TS Web and Application Development Services")
   end
   def create_dev
     first = (LdapLookup.get_simple_name(self.email.split("@").first)).split.first
